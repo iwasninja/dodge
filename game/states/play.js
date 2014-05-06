c653062293cdf3cebd947c2868a34827fac25914
Vagios Vlachos <bagios234@gmail.com>
2014-05-07 00:39:21 +0300

    Add arcade physics system on play screen

--- game/states/play.js
+++ game/states/play.js
@@ -3,24 +3,18 @@
   function Play() {}
   Play.prototype = {
     create: function() {
+      // Use the Arcade physics system
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
-      this.sprite.inputEnabled = true;
-      
-      this.game.physics.arcade.enable(this.sprite);
-      this.sprite.body.collideWorldBounds = true;
-      this.sprite.body.bounce.setTo(1,1);
-      this.sprite.body.velocity.x = this.game.rnd.integerInRange(-500,500);
-      this.sprite.body.velocity.y = this.game.rnd.integerInRange(-500,500);
+      this.game.physics.arcade.gravity.y = 500;
 
-      this.sprite.events.onInputDown.add(this.clickListener, this);
+      // Add background sprite (don't have one yet. Just color)
+      // this.background = this.game.add.sprite(0, 0, 'background');
+      
     },
     update: function() {
 
     },
-    clickListener: function() {
-      this.game.state.start('gameover');
-    }
+      
   };
   
   module.exports = Play;
