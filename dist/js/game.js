b83d0c2d774248bf544003f146f5d3011dcb01ec
Vagios Vlachos <bagios234@gmail.com>
2014-05-07 00:07:26 +0300

    Finish menu screen

--- dist/js/game.js
+++ dist/js/game.js
@@ -71,24 +71,56 @@ Menu.prototype = {
   preload: function() {
 
   },
+
   create: function() {
-    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
-    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
-    this.sprite.anchor.setTo(0.5, 0.5);
+    // Add background sprite (don't have one yet. Just color)
+    // this.background = this.game.add.sprite(0, 0, 'background');
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, '\'Allo, \'Allo!', style);
-    this.titleText.anchor.setTo(0.5, 0.5);
+    // Create wall group for ceiling, floor and round
+    this.wallGroup = this.game.add.group();
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
-    this.instructionsText.anchor.setTo(0.5, 0.5);
+    // Add ground and ceiling tileSprites
+    this.ceiling = this.game.add.tileSprite(0, 0, 480, 30, 'ground');
+    this.wallGroup.add(this.ceiling);
+    this.floor = this.game.add.tileSprite(0, this.game.height - 40, 480, 112, 'floor');
+    this.wallGroup.add(this.floor);
+    this.ground = this.game.add.tileSprite(0, this.game.height - 30, 480, 112, 'ground');
+    this.wallGroup.add(this.ground);
 
-    this.sprite.angle = -20;
-    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+    // Scroll the walls
+    this.ceiling.autoScroll(-100,0);
+    this.floor.autoScroll(-100, 0);
+    this.ground.autoScroll(-100,0);
+
+    // Create title group to manipulate
+    // title assets as one
+    this.titleGroup = this.game.add.group();
+
+    // Add dude to title group
+    this.dude = this.game.add.sprite(0, 0, 'dude');
+    this.titleGroup.add(this.dude);
+
+    // Set location of the group
+    this.titleGroup.x = 30;
+    this.titleGroup.y = 100;
+
+    // Animation tween to the titleGroup
+    this.game.add.tween(this.titleGroup).to({y:140}, 350, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+
+    // Add start button with a callabck
+    this.startButton = this.game.add.button(this.game.width/2, this.game.height/2, 'startButton', this.startClick, this);
+    this.startButton.anchor.setTo(0.5, 0.5);
+
   },
+
+  startClick: function() {
+    // Start button click handler
+    // Start the 'play' state
+    this.game.state.start('play');
+  },
+
   update: function() {
-    if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('play');
-    }
+
   }
 };
 
@@ -139,11 +171,19 @@ Preload.prototype = {
     // Demo asset
     this.load.image('yeoman', 'assets/yeoman-logo.png');
 
+    // Background color
+    this.stage.backgroundColor = '#87ceeb';
+
     // Preload assets
+    this.load.spritesheet('dudeFront', 'assets/square.png', 20, 20);
     this.load.spritesheet('dude', 'assets/square2.png', 40, 40);
+    this.load.image('ceiling', 'assets/ceil.png');
+    this.load.image('floor', 'assets/floor.png');
     this.load.image('ground', 'assets/ground.png');
     this.load.image('star', 'assets/shur.png');
     this.load.image('leftbo', 'assets/leftbor.png');
+
+    this.load.image('startButton', 'assets/start-button.png');
 
   },
   create: function() {
--- game/states/preload.js
+++ game/states/preload.js
@@ -15,11 +15,19 @@ Preload.prototype = {
     // Demo asset
     this.load.image('yeoman', 'assets/yeoman-logo.png');
 
+    // Background color
+    this.stage.backgroundColor = '#87ceeb';
+
     // Preload assets
+    this.load.spritesheet('dudeFront', 'assets/square.png', 20, 20);
     this.load.spritesheet('dude', 'assets/square2.png', 40, 40);
+    this.load.image('ceiling', 'assets/ceil.png');
+    this.load.image('floor', 'assets/floor.png');
     this.load.image('ground', 'assets/ground.png');
     this.load.image('star', 'assets/shur.png');
     this.load.image('leftbo', 'assets/leftbor.png');
+
+    this.load.image('startButton', 'assets/start-button.png');
 
   },
   create: function() {
