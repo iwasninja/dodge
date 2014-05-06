d21a80b7d6c0ca60dda103347dec040b52f9b07c
Vagios Vlachos <bagios234@gmail.com>
2014-05-06 16:58:15 +0300

    Preload assets

--- game/states/preload.js
+++ game/states/preload.js
@@ -1,5 +1,5 @@
-
 'use strict';
+
 function Preload() {
   this.asset = null;
   this.ready = false;
@@ -7,12 +7,19 @@ function Preload() {
 
 Preload.prototype = {
   preload: function() {
+    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.asset = this.add.sprite(this.width/2,this.height/2, 'preloader');
     this.asset.anchor.setTo(0.5, 0.5);
-
-    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
     this.load.setPreloadSprite(this.asset);
+
+    // Demo asset
     this.load.image('yeoman', 'assets/yeoman-logo.png');
+
+    // Preload assets
+    this.load.spritesheet('dude', 'assets/square2.png', 40, 40);
+    this.load.image('ground', 'assets/ground.png');
+    this.load.image('star', 'assets/shur.png');
+    this.load.image('leftbo', 'assets/leftbor.png');
 
   },
   create: function() {
