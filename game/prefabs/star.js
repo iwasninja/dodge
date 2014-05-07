41f3d8a45df4397547078838a4e6450a621990c8
Vagios Vlachos <bagios234@gmail.com>
2014-05-07 22:53:37 +0300

    Generate star prefab and initialize it

---
+++ game/prefabs/star.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Star = function(game, x, y, frame) {
+  Phaser.Sprite.call(this, game, x, y, 'star', frame);
+
+  // Set anchor and enable physics on star objects
+  this.anchor.setTo(0.5, 0.5);
+  this.game.physics.arcade.enableBody(this);
+
+  // Disable gravity and make them immovable
+  this.body.allowGravity = false;
+  this.body.immovable = true;
+
+  // Give velocity to star objects
+  this.body.velocity.x = -100;
+  
+};
+
+Star.prototype = Object.create(Phaser.Sprite.prototype);
+Star.prototype.constructor = Star;
+
+Star.prototype.update = function() {
+  
+  // write your prefab's specific update code here
+  
+};
+
+module.exports = Star;
