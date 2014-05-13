5363cb23a23c1b58e76d475716f63cae92dc4cca
iwasninja <sir_spa@yahoo.gr>
2014-05-13 18:26:05 +0300

    shuriken/porthole generator improved, dude's physics changed

--- game/states/preload.js
+++ game/states/preload.js
@@ -19,7 +19,6 @@ Preload.prototype = {
     this.load.spritesheet('dude', 'assets/square2.png', 40, 40);
     this.load.image('pipe', 'assets/pipe.png');
     this.load.image('floor', 'assets/floor.png');
-    this.load.image('ground', 'assets/floor.png');
     this.load.image('star', 'assets/shur.png');
     this.load.image('bricks', 'assets/bricks.jpg');
     this.load.image('porthole', 'assets/porthole.png');
